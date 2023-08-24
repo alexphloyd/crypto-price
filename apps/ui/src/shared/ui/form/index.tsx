6c104e431d31952d5d429486c3615e70df8a7b49
alexphloyd <alexphloyd@gmail.com>
2023-08-24 13:39:59 -0500

    migrate to class-validator

--- apps/ui/src/shared/ui/form/index.tsx
+++ apps/ui/src/shared/ui/form/index.tsx
@@ -1,35 +1,25 @@
-import { ZodType, z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { FormProvider, useForm } from 'react-hook-form';
-import { SubmitError } from './submit-error';
+import { ErrorLabel } from './error-label';
 import { FormProps } from './types';
 import { ForwardedRef, forwardRef } from 'react';
+import { classValidatorResolver } from '@hookform/resolvers/class-validator';
 
 import clsx from 'clsx';
 import { Button } from 'antd';
 import { Loader } from '../loader';
 
-export const Form = forwardRef(FormElement) as <S extends ZodType<any, any>>(
-  props: FormProps<S> & { ref?: ForwardedRef<HTMLFormElement> }
+export const Form = forwardRef(FormElement) as <S extends new (...args: any) => any>(
+  props: FormProps<S> & { ref?: ForwardedRef<HTMLFormElement> },
 ) => ReturnType<typeof FormElement>;
 
-function FormElement<S extends ZodType<any, any>>(
-  {
-    schema,
-    onSubmit,
-    errorMessage,
-    submitText,
-    children,
-    isLoading,
-    className,
-    ...props
-  }: FormProps<S>,
-  ref: ForwardedRef<HTMLFormElement>
+function FormElement<S extends new (...args: any) => any>(
+  { schema, onSubmit, errorMessage, submitText, children, isLoading, className, ...props }: FormProps<S>,
+  ref: ForwardedRef<HTMLFormElement>,
 ) {
-  const ctx = useForm<z.infer<S>>({
+  const ctx = useForm<InstanceType<S>>({
     mode: 'onSubmit',
     reValidateMode: 'onChange',
-    resolver: zodResolver(schema),
+    resolver: classValidatorResolver(schema),
   });
 
   const handleSubmit = ctx.handleSubmit(async (values) => {
@@ -38,33 +28,16 @@ function FormElement<S extends ZodType<any, any>>(
 
   return (
     <FormProvider {...ctx}>
-      <form
-        ref={ref}
-        onSubmit={handleSubmit}
-        className={clsx(className, 'flex flex-col gap-3')}
-        {...props}
-      >
+      <form ref={ref} onSubmit={handleSubmit} className={clsx(className, 'flex flex-col gap-3')} {...props}>
         {children}
 
         {submitText ? (
           <div className='flex flex-col items-center'>
-            <Button
-              htmlType='submit'
-              type='primary'
-              className='mt-[14px] w-full'
-              size='large'
-            >
-              {isLoading ? (
-                <Loader
-                  color='white'
-                  size='sm'
-                />
-              ) : (
-                submitText
-              )}
+            <Button htmlType='submit' type='primary' className='mt-[14px] w-full' size='large'>
+              {isLoading ? <Loader color='white' size='sm' /> : submitText}
             </Button>
 
-            <SubmitError message={errorMessage} />
+            <ErrorLabel message={errorMessage} />
           </div>
         ) : null}
       </form>
--- apps/ui/src/shared/ui/form/types.ts
+++ apps/ui/src/shared/ui/form/types.ts
@@ -1,24 +1,20 @@
 import { PropsWithoutRef, ReactNode } from 'react';
-import { UseFormProps } from 'react-hook-form';
 import { OmitStrict } from '@utility-types';
-import { ZodAny, z } from 'zod';
 
 export interface OnSubmitResult {
   FORM_ERROR?: string;
   [prop: string]: unknown;
 }
 
-export interface FormProps<S extends z.ZodType<ZodAny, ZodAny>>
+export interface FormProps<S extends new (...args: any) => any>
   extends OmitStrict<PropsWithoutRef<JSX.IntrinsicElements['form']>, 'onSubmit'> {
   schema: S;
-  onSubmit: (values: z.infer<S>) => Promise<void | OnSubmitResult>;
+  onSubmit: (values: InstanceType<S>) => Promise<void | OnSubmitResult>;
   isLoading?: boolean;
   errorMessage?: string | null | undefined;
   submitText?: string | undefined;
-  additionalStyles?: string | undefined;
   children?: ReactNode | undefined;
   className?: string;
-  initialValues?: UseFormProps<z.infer<S>>['defaultValues'];
 }
 
 export const FORM_ERROR = 'FORM_ERROR';
