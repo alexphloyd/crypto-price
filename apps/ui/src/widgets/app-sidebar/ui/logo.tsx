a825142d22e6570d1f7df7d4b849c4f8a8539008
alexphloyd <alexphloyd@gmail.com>
2023-08-24 17:48:50 -0500

    update app-sidebar styles

--- apps/ui/src/widgets/app-sidebar/ui/logo.tsx
+++ apps/ui/src/widgets/app-sidebar/ui/logo.tsx
@@ -1,10 +1,11 @@
+import { Typography } from 'antd';
 import clsx from 'clsx';
 
 export const AppLogo = ({ className }: { className?: string }) => {
   return (
     <main className={clsx('flex items-center', className)}>
-      <img src="/logo.png" alt="app-logo" className="w-7 h-7 mr-[10px]" />
-      <span className="tracking-wide text-xl font-mono">sync-vision</span>
+      <img src='/logo.png' alt='app-logo' className='w-7 h-7 mr-[10px]' />
+      <Typography.Text className='tracking-wider text-xl'>sync-vision</Typography.Text>
     </main>
   );
 };
