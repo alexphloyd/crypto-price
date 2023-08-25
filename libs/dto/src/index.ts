57c535c8ad9dee334ffa28f8585859a66610e13f
alexphloyd <alexphloyd@gmail.com>
2023-08-25 11:20:25 -0500

    add utils lib

--- libs/dto/src/index.ts
+++ libs/dto/src/index.ts
@@ -1,4 +1,5 @@
-export { CreateUserDto, type CreateUserInput } from './auth/create-user.dto';
+export { SignUpDto, type SignUpInput } from './auth/sign-up.dto';
+
 export { VerifyUserDto, type VerifyUserInput } from './auth/verify-user.dto';
 
 export { LoginDto, type LoginInput } from './auth/login.dto';
