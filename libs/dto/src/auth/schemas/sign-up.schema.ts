1096a52a66266399f047da2760e6d38c8112e19b
alexphloyd <alexphloyd@gmail.com>
2023-09-05 17:02:13 +0200

    update sign-up schema

--- libs/dto/src/auth/schemas/sign-up.schema.ts
+++ libs/dto/src/auth/schemas/sign-up.schema.ts
@@ -1,17 +1,17 @@
-import { NotificationProvider, type NotificationProvider as PossibleNotificationProviders } from '@prisma/client';
 import { z } from 'zod';
+import { NotificationProvider } from '@prisma/client';
+import { createUnionSchema } from '@utils/zod/create-union';
 
-const NotificationProviders = Object.values(NotificationProvider);
+const NotificationProviderUnion = createUnionSchema(
+  [NotificationProvider.MESSENGER, NotificationProvider.TELEGRAM, NotificationProvider.WHATSAPP] as const,
+  { message: 'must be specified' },
+);
 
 export const SignUpSchema = z.object({
   name: z.string().min(2, { message: 'must be longer' }).max(30, { message: 'must be shorter' }),
   surname: z.string().min(2, { message: 'must be longer' }).max(30, { message: 'must be shorter' }),
   email: z.string().min(1, { message: 'must be longer' }).email({ message: 'must be valid' }),
   password: z.string().min(6, { message: 'must be longer' }),
-  notificationProvider: z
-    .string()
-    .refine((arg) => NotificationProviders.includes(arg as PossibleNotificationProviders), {
-      message: 'must be specified',
-    }),
   phoneNumber: z.string().min(7, { message: 'must be valid' }).max(15, { message: 'must be valid' }),
+  notificationProvider: NotificationProviderUnion,
 });
--- libs/utils/src/zod/create-union.ts
+++ libs/utils/src/zod/create-union.ts
@@ -1,13 +1,14 @@
 import { type Primitive, z, type ZodLiteral, ZodNever } from 'zod';
 
+type Options = {
+  message?: string;
+};
+
 type MappedZodLiterals<T extends readonly Primitive[]> = {
   -readonly [K in keyof T]: ZodLiteral<T[K]>;
 };
 
-function createManyUnion<A extends readonly [Primitive, Primitive, ...Primitive[]]>(
-  literals: A,
-  { message }: { message?: string },
-) {
+function createManyUnion<A extends readonly [Primitive, Primitive, ...Primitive[]]>(literals: A, { message }: Options) {
   return z.union(
     literals.map((value) =>
       z.literal(value, { errorMap: message ? () => ({ message }) : undefined }),
@@ -15,14 +16,11 @@ function createManyUnion<A extends readonly [Primitive, Primitive, ...Primitive[
   );
 }
 
-export function createUnionSchema<T extends readonly []>(values: T, { message }: { message?: string }): ZodNever;
-export function createUnionSchema<T extends readonly [Primitive]>(
-  values: T,
-  { message }: { message?: string },
-): ZodLiteral<T[0]>;
+export function createUnionSchema<T extends readonly []>(values: T, { message }: Options): ZodNever;
+export function createUnionSchema<T extends readonly [Primitive]>(values: T, { message }: Options): ZodLiteral<T[0]>;
 export function createUnionSchema<T extends readonly [Primitive, Primitive, ...Primitive[]]>(
   values: T,
-  { message }: { message?: string },
+  { message }: Options,
 ): z.ZodUnion<MappedZodLiterals<T>>;
 export function createUnionSchema<T extends readonly Primitive[]>(values: T, { message }: { message?: string }) {
   if (values.length > 1) {
